104372f3c70c64266322e7be2f1f09e3ea1d7309
Lukasz <lukasz@lukszp.(none)>
2009-12-09 02:34:44 +0800

    Change log:
    - settings.py updated (admin emails, language)
    - book/admin.py file created and setup for Joke model

---
+++ book/admin.py
@@ -0,0 +1,4 @@
+from jokebook.book.models import Joke
+from django.contrib import admin
+
+admin.site.register(Joke)
--- settings.py
+++ settings.py
@@ -7,6 +7,9 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
+    ('Tomasz Filipczuk','tomek.filipczuk@gmail.com'),
+    ('Lukasz Szpak','lukasz.szpak@gmail.com'),
+    ('Sebastian Pawlus','sebastian.pawlus@gmail.com'),
     # ('Your Name', 'your_email@domain.com'),
 )
 
@@ -24,11 +27,11 @@ DATABASE_PORT = ''             # Set to empty string for default. Not used with 
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Poland/Warsaw'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl'
 
 SITE_ID = 1
 
